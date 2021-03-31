ff63446660a127bdecfa72198b35026b3dbbda0d
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-03-30 22:45:01 -0300

    Criacao de rotas para navegacao das paginas

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {ClientesRoutingModule} from './clientes-routing.module';
 import {ClientesComponent} from './clientes.component'
 
 @NgModule({
@@ -8,7 +9,8 @@ import {ClientesComponent} from './clientes.component'
     ClientesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ClientesRoutingModule
   ],
   exports: [
     ClientesComponent
